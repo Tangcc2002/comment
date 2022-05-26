8b37ac20e5200a4d4ae4fc634363accb95d85a89
tang <2856440943@qq.com>
2022-05-26 21:04:19 +0800

    增加了所有用户的功能

--- src/main/java/com/comment/controller/UserController.java
+++ src/main/java/com/comment/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,5 +17,10 @@ public class UserController {
     @GetMapping("/user/{id}")
     public User findUser(@PathVariable("id") Integer id) {
         return service.getById(id);
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+        return service.list();
     }
 }
